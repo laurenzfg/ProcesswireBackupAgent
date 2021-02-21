3fcaa3a0c068c0c2de0367fb2bf635f7b5534f9d
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-02-21 12:23:23 +0100

    Refactor Dumping Code into Function

--- make_sql_dump.php
+++ make_sql_dump.php
@@ -2,42 +2,49 @@
 
 ini_set('max_execution_time', 300);
 
-// DB Login Data Goes Here
+// DB and AWS credentials are saved there
+include_once 'secrets.php';
 
-include_once 'secrets.php'
+function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword) {
+    // Call to the system's mysqldump util
+    $command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ../../dump.sql';
+    exec($command,$output=array(),$worked);
+    switch($worked) { // check return code
+        case 1:
+            echo 'Could not make SQL dump'. PHP_EOL;
+            return -1;
+            break;
+        case 2:
+            echo 'Could not make SQL dump due to DB connection issues.'. PHP_EOL;
+            return -1;
+            break;
+    }
 
-//Bei den folgenden Punkten bitte keine Änderung durchführen
-//Export der Datenbank und Ausgabe des Status
-$command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ../dump.sql';
-exec($command,$output=array(),$worked);
-switch($worked){
-    case 0:
-        break;
-    case 1:
-        echo 'Could not make SQL dump'. PHP_EOL;
-        break;
-    case 2:
-        echo 'Could not make SQL dump due to DB connection issues.'. PHP_EOL;
-        break;
-}
-
-exec('gzip -f ../dump.sql',$output=array(),$worked);
-switch($worked){
-    case 0:
-        break;
-    default:
+    // Gzip the SQL dump cause SQL dumps are just a long sequence of SQL statements
+    // that is very inefficent. GZIP can help us a lot!
+    // SIDE EFFECT: gzip removes the (uncompressed) file! Remainder: {filename}.gz
+    exec('gzip -f ../../dump.sql',$output=array(),$worked);
+    if ($worked > 0) { // check return code
         echo 'Could not compress SQL dump'. PHP_EOL;
-        break;
-}
+        return -1;
+    }
 
-exec('tar -zcvf ../site.tgz ./site',$output=array(),$worked);
-switch($worked){
-    case 0:
-        break;
-    default:
+    // Assets etc. are saved in the 'site' folder
+    // We make a GZIP compressed tar of everything
+    exec('tar -zvc -C ../ -f ../../site.tgz site',$output=array(),$worked);
+    if ($worked > 0) { // check return code
         echo 'Could not make site folder dump'. PHP_EOL;
-        break;
+        return -1;
+    }
+    return 0;
+
 }
 
-echo 'Script completed.'. PHP_EOL;
+$retval = make_backup_files($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword);
+if ($retval == 0) {
+    echo 'Backup completed.'. PHP_EOL;
+} else {
+    echo 'Backup unsuccessful.'. PHP_EOL;
+}
+
 ?>
