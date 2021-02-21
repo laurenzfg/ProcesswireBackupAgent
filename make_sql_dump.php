de8630fac3a750e3c3f0c79f56c2a7b91f90af70
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-02-21 12:41:45 +0100

    Prefix files with current time

--- make_sql_dump.php
+++ make_sql_dump.php
@@ -5,9 +5,15 @@ ini_set('max_execution_time', 300);
 // DB and AWS credentials are saved there
 include_once 'secrets.php';
 
-function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword) {
+// Choose UNIX time as the prefix for all files
+$prefix = time();
+
+function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword, $filenamePrefix) {
+    $dumpname = $filenamePrefix . '_sqldump.sql';
+    $sitetarname = $filenamePrefix . '_site.tgz';
+    
     // Call to the system's mysqldump util
-    $command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ../../dump.sql';
+    $command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ../../' . $dumpname;
     exec($command,$output=array(),$worked);
     switch($worked) { // check return code
         case 1:
@@ -23,7 +29,7 @@ function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, 
     // Gzip the SQL dump cause SQL dumps are just a long sequence of SQL statements
     // that is very inefficent. GZIP can help us a lot!
     // SIDE EFFECT: gzip removes the (uncompressed) file! Remainder: {filename}.gz
-    exec('gzip -f ../../dump.sql',$output=array(),$worked);
+    exec('gzip -f ../../'.$dumpname,$output=array(),$worked);
     if ($worked > 0) { // check return code
         echo 'Could not compress SQL dump'. PHP_EOL;
         return -1;
@@ -31,7 +37,7 @@ function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, 
 
     // Assets etc. are saved in the 'site' folder
     // We make a GZIP compressed tar of everything
-    exec('tar -zvc -C ../ -f ../../site.tgz site',$output=array(),$worked);
+    exec('tar -zvc -C ../ -f ../../' . $sitetarname . ' site',$output=array(),$worked);
     if ($worked > 0) { // check return code
         echo 'Could not make site folder dump'. PHP_EOL;
         return -1;
@@ -40,7 +46,7 @@ function make_backup_files ($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, 
 
 }
 
-$retval = make_backup_files($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword);
+$retval = make_backup_files($mysqlHostName, $mysqlDatabaseName, $mysqlUserName, $mysqlPassword, $prefix);
 if ($retval == 0) {
     echo 'Backup completed.'. PHP_EOL;
 } else {
